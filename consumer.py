3f145a0535f943373a01eacf686dbcc34ecfebce
Peter Szabo <peter.szabo@student.hogent.be>
2022-09-22 22:07:48 +0200

    Added an outout message, with some details and prediction result

--- consumer.py
+++ consumer.py
@@ -7,25 +7,31 @@ import numpy as np
 # load the model from disk
 loaded_model = pickle.load(open('model.sav', 'rb'))
 
-image = '3.png'
+actualValue = 4
+usedK = 5
+image = '4.png'
 img = Image.open(image).convert('L') # L ... greyscale
 #
 
 myImage = img.resize((28, 28))
-
+inverted = False
 
 if np.average(np.reshape(np.array(myImage), (28 * 28))) > 128.:
     myImage = ImageOps.invert(myImage)
+    inverted = True
 
 shaped = np.reshape(np.array(myImage), (28 * 28))
 
-print(np.average(shaped))
+avgPixel = np.average(shaped)
 
 
 pyplot.imshow(myImage, cmap=pyplot.get_cmap('gray'))
 pyplot.show()
 
 
-print(loaded_model.predict([shaped]))
+predictions = loaded_model.predict([shaped])
 
-
+for id, score in enumerate(predictions[0]):
+    if score == 1:
+        print('Actual number:', actualValue,', prediction of knn model with k=', usedK, ' is:', id,'. Average pixel '
+                'darkness (out of 255):', avgPixel,', Inverted:', inverted)
