01e466048987ad7f6b5252b5ca18edb3e11da277
die-Anna <anna.stefanie.matt@gmail.com>
2022-09-22 20:33:38 +0200

    added optional inversion

--- consumer.py
+++ consumer.py
@@ -1,22 +1,31 @@
 import pickle
 import matplotlib.pyplot as pyplot
 from PIL import Image
+from PIL import ImageOps
 import numpy as np
 
 # load the model from disk
 loaded_model = pickle.load(open('model.sav', 'rb'))
 
-image = 'images.png'
+image = '3.png'
 img = Image.open(image).convert('L') # L ... greyscale
+#
 
 myImage = img.resize((28, 28))
 
 
+if np.average(np.reshape(np.array(myImage), (28 * 28))) > 128.:
+    myImage = ImageOps.invert(myImage)
+
 shaped = np.reshape(np.array(myImage), (28 * 28))
+
+print(np.average(shaped))
+
+
 pyplot.imshow(myImage, cmap=pyplot.get_cmap('gray'))
 pyplot.show()
 
 
-print(loaded_model.predict([shaped, shaped]))
+print(loaded_model.predict([shaped]))
 
 
