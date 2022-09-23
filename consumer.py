021f75eac6d9c08a603483f32ae2ecaf68e298ea
m047 <ai22m047@technikum-wien.at>
2022-09-23 10:06:33 +0200

    added comments, removed unnecessary lines, commented out print messages

--- consumer.py
+++ consumer.py
@@ -10,28 +10,30 @@ loaded_model = pickle.load(open('model.sav', 'rb'))
 actualValue = 4
 usedK = 5
 image = '4.png'
-img = Image.open(image).convert('L') # L ... greyscale
-#
 
+# load image and fit it to match our training data (28x28 px, grayscale, white line on black background)
+# convert('L') converts the image into grayscale
+img = Image.open(image).convert('L')
 myImage = img.resize((28, 28))
+# invert colors if image has more whites than blacks (e.g. if it has a black line on white background)
 inverted = False
-
 if np.average(np.reshape(np.array(myImage), (28 * 28))) > 128.:
     myImage = ImageOps.invert(myImage)
     inverted = True
 
+# put 2d pixel values into one dimension
 shaped = np.reshape(np.array(myImage), (28 * 28))
 
+# calculate brightness mean
 avgPixel = np.average(shaped)
 
+# code for showing the preprocessed image
+# pyplot.imshow(myImage, cmap=pyplot.get_cmap('gray'))
+# pyplot.show()
 
-pyplot.imshow(myImage, cmap=pyplot.get_cmap('gray'))
-pyplot.show()
-
-
+# output prediction
 predictions = loaded_model.predict([shaped])
-
-for id, score in enumerate(predictions[0]):
+for index, score in enumerate(predictions[0]):
     if score == 1:
-        print('Actual number:', actualValue,', prediction of knn model with k=', usedK, ' is:', id,'. Average pixel '
-                'darkness (out of 255):', avgPixel,', Inverted:', inverted)
+        print('Actual number:', actualValue, ', prediction of knn model with k=', usedK, ' is:', index,
+              '. Average pixel darkness (out of 255):', avgPixel, ', Inverted:', inverted)
--- main.py
+++ main.py
@@ -6,34 +6,28 @@ from tensorflow.python.keras.utils.np_utils import to_categorical
 import pickle
 
 data = mnist.load_data()
-# print(data)
 (X_train, y_train), (X_test, y_test) = data
 
-for i in range(2):
-    pyplot.subplot(330 + 1 + i)
-    pyplot.imshow(X_train[i], cmap=pyplot.get_cmap('gray'))
-    pyplot.show()
+# code for showing all images in a range
+# for i in range(2):
+#    pyplot.subplot(330 + 1 + i)
+#    pyplot.imshow(X_train[i], cmap=pyplot.get_cmap('gray'))
+#    pyplot.show()
 
-# reshape(6000, 28 * 28)
-print(X_train.shape)
+# reshape data (put 2d pixel values into one dimension)
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
-print(X_train.shape)
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-
-
-
-model = neighbors.KNeighborsClassifier(n_neighbors=5)
-
+k_value = 5
+model = neighbors.KNeighborsClassifier(n_neighbors=k_value)
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
 # evaluate the model and update the accuracies list
 score = model.score(X_test, y_test)
-print("k=%d, accuracy=%.2f%%" % (5, metrics.accuracy_score(y_test, predictions) * 100))
-print("k=%d, f1_score=%.2f%%" % (5, metrics.f1_score(y_test, predictions, average="weighted") * 100))
+# code for accuracy output
+# print("k=%d, accuracy=%.2f%%" % (k_value, metrics.accuracy_score(y_test, predictions) * 100))
+# print("k=%d, f1_score=%.2f%%" % (k_value, metrics.f1_score(y_test, predictions, average="weighted") * 100))
 
 # save model to file
 filename = 'model.sav'
