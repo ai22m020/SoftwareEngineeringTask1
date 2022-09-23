de64519eab2506672e45b2ba9703593fa0559dc1
die-Anna <anna.stefanie.matt@gmail.com>
2022-09-23 10:03:17 +0200

    added command line argument

--- consumer.py
+++ consumer.py
@@ -3,25 +3,38 @@ import matplotlib.pyplot as pyplot
 from PIL import Image
 from PIL import ImageOps
 import numpy as np
+import sys
+
+
+# define image size and greyscale limit
+width = 28
+height = 28
+greyscale_limit = 128
+
 
 # load the model from disk
 loaded_model = pickle.load(open('model.sav', 'rb'))
 
-image = '3.png'
-img = Image.open(image).convert('L') # L ... greyscale
-#
+# read image path from command line argument
+if len(sys.argv) > 1:
+    image = sys.argv[1]
+else:
+    image = '4.png'
 
-myImage = img.resize((28, 28))
+# open image in greyscale mode ('L')
+img = Image.open(image).convert('L')
 
+# resize image to fit training data
+myImage = img.resize((width, height))
 
-if np.average(np.reshape(np.array(myImage), (28 * 28))) > 128.:
+# calculate average and invert colors if necessary
+if np.average(np.reshape(np.array(myImage), (width * height))) > greyscale_limit:
     myImage = ImageOps.invert(myImage)
 
-shaped = np.reshape(np.array(myImage), (28 * 28))
+# reshape image
+shaped = np.reshape(np.array(myImage), (width * height))
 
-print(np.average(shaped))
-
-
+# plot image
 pyplot.imshow(myImage, cmap=pyplot.get_cmap('gray'))
 pyplot.show()
 
